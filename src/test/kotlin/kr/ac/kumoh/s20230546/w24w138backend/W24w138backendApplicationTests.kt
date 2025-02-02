a8c6548333d6f67898a69030ed0ad12bf71aa1dc
seo-bo <fgxhmd@gmail.com>
2025-02-02 16:49:08 +0900

    Squashed 'w24w138backend/' content from commit 85da0d0
    git-subtree-dir: w24w138backend
    git-subtree-split: 85da0d0880362251062c3cc43dc4fd84b09ee7ea

---
+++ src/test/kotlin/kr/ac/kumoh/s20230546/w24w138backend/W24w138backendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230546.w24w138backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w138backendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
