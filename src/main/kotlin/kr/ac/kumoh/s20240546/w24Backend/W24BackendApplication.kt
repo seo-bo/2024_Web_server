0f1b23b664b5f60ab7a95306a41e8a962686dc3d
seo-bo <fgxhmd@gmail.com>
2025-02-02 16:49:03 +0900

    Squashed 'w24Backend/' content from commit d002c83
    git-subtree-dir: w24Backend
    git-subtree-split: d002c836ee4d9837b50849736ca81a3ff02ed65b

---
+++ src/main/kotlin/kr/ac/kumoh/s20240546/w24Backend/W24BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20240546.w24Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24BackendApplication>(*args)
+}
