9304cf6fbe0818c1d2e6e66caaddf34b61d96786
seo-bo <fgxhmd@gmail.com>
2025-02-02 16:48:46 +0900

    Squashed 'w24w11-song-list/' content from commit e01b64d
    git-subtree-dir: w24w11-song-list
    git-subtree-split: e01b64d110bc057ada2e04fdcf614bef6632cf93

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Container from './Container.js'
+
+const SERVER_URL = 'http://localhost:8080/api/songs'
+
+const App = () => {
+ const [songs, setSongs] = useState([])
+ const getSong = async () =>{
+    try {
+      const res = await axios.get(SERVER_URL)
+      console.log(res)
+      setSongs(res.data)
+    } catch (err){
+      console.log(err)
+      setSongs([])
+    }
+ }
+ useEffect(() => {
+  getSong()
+ },[])
+
+  return (
+    <div>
+      <Header/>
+      <Playlist 
+        title="프로그래밍하면서 듣고 싶은 노래"
+        listSong={songs}/>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+      <h1>React 프로그래밍</h1>
+  )
+}
+
+const Playlist = ({title,listSong}) => {
+  return (
+    <div className='playlist'>
+      <div className="playlist">{title}</div>
+      {
+        listSong.map(song => 
+          <Container key={song.id} song={song}/>
+        )
+      }
+    </div>
+  )
+}
+
+export default App
